ce97692292d0c3a31cf2bc36334af7d513e2e6be
Marc Ford <marc@pigeon.ly>
2019-12-25 15:50:12 -0800

    made the build script a little more user friendly

--- scripts/build_web_app.sh
+++ scripts/build_web_app.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo "Building the web app now"
 cd web
+[ ! -e web ] || rm web
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o web .
+if [ $? -eq 0 ]
+then
+    echo "Web App built to start the server run 'make start_srv'"
+else
+    echo "There was a problem building the web app!"
+fi
