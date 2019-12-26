966d3a332084ec0b40adf3d6b3988868e7225dae
Marc Ford <marc@pigeon.ly>
2019-12-25 17:13:48 -0800

    doing some things with middleware working on making logging better ping now responds with up time also

--- web/go.mod
+++ web/go.mod
@@ -3,6 +3,7 @@ module github.com/MarcFord/Go-Go-Gadget-Website/web
 go 1.13
 
 require (
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/sirupsen/logrus v1.4.2
 )
--- web/main.go
+++ web/main.go
@@ -5,27 +5,60 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 	log "github.com/sirupsen/logrus"
+	"net"
 	"net/http"
 	"time"
 )
 
 type Pong struct {
 	Message string `json:"message"`
-	Timestamp string `json:"timestamp"`
+	Timestamp string `json:"now"`
+	UpTime string `json:"up_time""`
 }
 
+var startedAt time.Time
+
 func pingHandler(response http.ResponseWriter, request *http.Request) {
-	// Set Response Header
-	response.Header().Set("Content-Type", "application/json")
 	var pong Pong
 	pong.Message = "Pong"
 	pong.Timestamp = time.Now().UTC().Format(time.RFC850)
-	log.Debug(fmt.Sprintf("Index Route Hit from %v", request.RemoteAddr))
+	pong.UpTime = fmt.Sprintf("%v", time.Now().UTC().Sub(startedAt.UTC()))
 	json.NewEncoder(response).Encode(pong)
 }
 
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		host, _, err := net.SplitHostPort(r.RemoteAddr)
+
+		if err != nil {
+			host = r.RemoteAddr
+		}
+		log.WithFields(log.Fields{
+			"client_ip": host,
+			"uri":   r.RequestURI,
+			"user-agent": r.UserAgent(),
+			"referer": r.Referer(),
+		}).Info(fmt.Sprintf("Request Made for %s", r.RequestURI))
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		w.Header().Set("Content-Type", "application/json")
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main()  {
+	// set start time
+	startedAt = time.Now()
 	// Configure Logging
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
@@ -43,8 +76,12 @@ func main()  {
 	// Route handlers
 	router.HandleFunc("/ping", pingHandler).Methods("GET")
 
+	// Setup logged Router
+	router.Use(loggerMiddleware)
+	router.Use(jsonMiddleware)
+
 	// Run Server
 	address := fmt.Sprintf("%s:%d", *srvAddressPtr, *portPtr)
 	log.Info(fmt.Sprintf("Server starting on http://%s\n", address))
-	log.Fatal(http.ListenAndServe(address, router))
+	log.Fatal(http.ListenAndServe(address, handlers.CORS()(router)))
 }
